b7193ce7ee2a3f0f40557003e8cf766012933238
G R S S SRI LALITHA <lalithagoparaju77@gmail.com>
2024-10-15 11:47:49 +0530

    Delete AWESOME CHCOLATES COMPANY DATA ANALYSIS USING SQL/PEOPLE QUERY.sql

--- AWESOME CHCOLATES COMPANY DATA ANALYSIS USING SQL/PEOPLE QUERY.sql
+++
@@ -1,9 +0,0 @@
-select * from people;
-
-select * from people where team = 'Delish' or team = 'Jucies';
-
-select * from people where team in ('Delish','Jucies');
-
-select * from people where Salesperson like 'B%';
-
-select * from people where Salesperson like '%B%';
